4b7f6d88a96253c354280b71d223f301026c22c4
Clint Losee <clint.losee@gmail.com>
2016-05-11 08:40:40 -0600

    Added graphical elements and connected them in viewcontroller.

--- tapper/tapper/ViewController.swift
+++ tapper/tapper/ViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
